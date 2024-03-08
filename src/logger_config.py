105cb19b0f46c7fc2c3f73b1ba0fa9db5cc30c71
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-03-08 13:32:40 +0300

    logger added

---
+++ src/logger_config.py
@@ -0,0 +1,13 @@
+import logging
+
+def configure_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - : %(message)s', datefmt='%H:%M:%S')    
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+logger = configure_logger()
