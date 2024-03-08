73885134d2fda8f44c69f72700ed3d03e7727c7b
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-03-08 13:34:06 +0300

    test script added

---
+++ src/script.py
@@ -0,0 +1,12 @@
+
+
+# comment
+print("Fucked script !!!22312d fasd fsd f")
+print("Fucked script !!!22312d fasd fsd fsad falksjdfhalsdhf alskdhf"
+      + ' asldkfh aslkdfhasldkf hasldkfh alkshdf'
+      + ' laksdhf alsdhf laksdhf laskdhf lakshdfl ashdf')
+
+
+def foobar(msg: str) -> str:
+    _ = f'hello, {msg}'
+    return 'deadbeef'
