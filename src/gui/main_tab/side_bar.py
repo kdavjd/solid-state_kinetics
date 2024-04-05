ded72703a1c0d26c609c4b1001c68402d4a63370
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-04-05 12:10:34 +0300

    mark_as_active added

--- src/gui/main_tab/side_bar.py
+++ src/gui/main_tab/side_bar.py
@@ -4,12 +4,14 @@ from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtGui import QStandardItem, QStandardItemModel
 from PyQt6.QtWidgets import QTreeView, QVBoxLayout, QWidget
 
+from src.core.logger_config import logger
+
 from .load_file_button import LoadButton
 
 
 class SideBar(QWidget):
     file_selected = pyqtSignal(tuple)
-    sub_side_bar_needed = pyqtSignal(bool)
+    sub_side_bar_needed = pyqtSignal(str)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -40,16 +42,40 @@ class SideBar(QWidget):
         self.load_button = LoadButton(self)
         self.load_button.file_selected.connect(self.add_experiment_file)
 
+        self.active_file_item = None
+
+    def mark_as_active(self, item):
+        if self.active_file_item:
+            self.unmark_active_state(self.active_file_item)
+        self.mark_active_state(item)
+        self.active_file_item = item
+        logger.debug(f"Активный файл: {item.text()}")
+
+    def mark_active_state(self, item):
+        font = item.font()
+        font.setBold(True)
+        item.setFont(font)
+
+    def unmark_active_state(self, item):
+        font = item.font()
+        font.setBold(False)
+        item.setFont(font)
+
     def on_item_clicked(self, index):
         item = self.model.itemFromIndex(index)
         if item == self.add_data_item:
             self.load_button.open_file_dialog()
-        if item.parent() == self.experiments_data_root:
-            self.sub_side_bar_needed.emit(True)
+        elif item.parent() == self.experiments_data_root:
+            self.sub_side_bar_needed.emit(item.text())
+            self.mark_as_active(item)
+        elif item.parent() == self.calculation_root:
+            self.sub_side_bar_needed.emit(item.text())
         else:
-            self.sub_side_bar_needed.emit(False)
+            self.sub_side_bar_needed.emit("")
 
     def add_experiment_file(self, file_info):
         new_file_item = QStandardItem(path.basename(file_info[0]))
         self.experiments_data_root.insertRow(self.experiments_data_root.rowCount() - 1, new_file_item)
         self.tree_view.expandAll()
+        self.mark_as_active(new_file_item)
+        logger.debug(f"New file added and marked as active: {new_file_item.text()}")
