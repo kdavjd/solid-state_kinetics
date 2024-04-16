6a4641bfaea9e4e95a0521692852d6bede82168d
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-04-16 14:56:55 +0300

    initialize_sizes приведена к размеру компонентов

--- src/gui/main_tab/main_tab.py
+++ src/gui/main_tab/main_tab.py
@@ -8,9 +8,12 @@ from .sub_sidebar.sub_side_hub import SubSideHub
 
 MIN_WIDTH_SIDEBAR = 220
 MIN_WIDTH_SUBSIDEBAR = 200
-MIN_HEIGHT_CONSOLE = 150
-MIN_HEIGHT_PLOTCANVAS = 500
+MIN_WIDTH_CONSOLE = 150
+MIN_WIDTH_PLOTCANVAS = 500
+SPLITTER_WIDTH = 100
 MIN_HEIGHT_MAINTAB = 500
+COMPONENTS_MIN_WIDTH = (
+    MIN_WIDTH_SIDEBAR + MIN_WIDTH_SUBSIDEBAR + MIN_WIDTH_CONSOLE + MIN_WIDTH_PLOTCANVAS + SPLITTER_WIDTH)
 
 
 class MainTab(QWidget):
@@ -18,6 +21,7 @@ class MainTab(QWidget):
         super().__init__(parent)
         self.layout = QVBoxLayout(self)
         self.setMinimumHeight(MIN_HEIGHT_MAINTAB)
+        self.setMinimumWidth(COMPONENTS_MIN_WIDTH + SPLITTER_WIDTH)
 
         self.splitter = QSplitter(Qt.Orientation.Horizontal, self)
         self.layout.addWidget(self.splitter)
@@ -32,26 +36,31 @@ class MainTab(QWidget):
         self.splitter.addWidget(self.sub_sidebar)
 
         self.plot_canvas = PlotCanvas(self)
-        self.plot_canvas.setMinimumHeight(MIN_HEIGHT_PLOTCANVAS)
+        self.plot_canvas.setMinimumWidth(MIN_WIDTH_PLOTCANVAS)
         self.splitter.addWidget(self.plot_canvas)
 
         self.console_widget = ConsoleWidget(self)
-        self.console_widget.setMinimumHeight(MIN_HEIGHT_CONSOLE)
+        self.console_widget.setMinimumWidth(MIN_WIDTH_CONSOLE)
         self.splitter.addWidget(self.console_widget)
 
         self.sidebar.sub_side_bar_needed.connect(self.toggle_sub_sidebar)
 
     def initialize_sizes(self):
         total_width = self.width()
-        sidebar_width = int(total_width / 5)
-        console_width = int(total_width / 5)
+
+        sidebar_ratio = MIN_WIDTH_SIDEBAR / COMPONENTS_MIN_WIDTH
+        subsidebar_ratio = MIN_WIDTH_SUBSIDEBAR / COMPONENTS_MIN_WIDTH
+        console_ratio = MIN_WIDTH_CONSOLE / COMPONENTS_MIN_WIDTH
+
+        sidebar_width = int(total_width * sidebar_ratio)
+        console_width = int(total_width * console_ratio)
 
         if self.sub_sidebar.isVisible():
-            sub_sidebar_width = int(total_width / 6)
-            canvas_width = int(total_width - sidebar_width - sub_sidebar_width - console_width)
+            sub_sidebar_width = int(total_width * subsidebar_ratio)
+            canvas_width = total_width - (sidebar_width + sub_sidebar_width + console_width)
             self.splitter.setSizes([sidebar_width, sub_sidebar_width, canvas_width, console_width])
         else:
-            canvas_width = int(total_width - sidebar_width - console_width)
+            canvas_width = total_width - (sidebar_width + console_width)
             self.splitter.setSizes([sidebar_width, 0, canvas_width, console_width])
 
     def showEvent(self, event):
