a5a6b5140946e24fc061a83da27947b9e2aecf81
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-04-16 13:07:47 +0300

    минимальные размеры вынесены в константы

--- src/gui/main_tab/main_tab.py
+++ src/gui/main_tab/main_tab.py
@@ -6,30 +6,37 @@ from .plot_canvas import PlotCanvas
 from .sidebar import SideBar
 from .sub_sidebar.sub_side_hub import SubSideHub
 
+MIN_WIDTH_SIDEBAR = 220
+MIN_WIDTH_SUBSIDEBAR = 200
+MIN_HEIGHT_CONSOLE = 150
+MIN_HEIGHT_PLOTCANVAS = 500
+MIN_HEIGHT_MAINTAB = 500
+
 
 class MainTab(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.layout = QVBoxLayout(self)
+        self.setMinimumHeight(MIN_HEIGHT_MAINTAB)
 
         self.splitter = QSplitter(Qt.Orientation.Horizontal, self)
         self.layout.addWidget(self.splitter)
 
         self.sidebar = SideBar(self)
-        self.sidebar.setMinimumWidth(220)
+        self.sidebar.setMinimumWidth(MIN_WIDTH_SIDEBAR)
         self.splitter.addWidget(self.sidebar)
 
         self.sub_sidebar = SubSideHub(self)
-        self.sub_sidebar.setMinimumWidth(200)
+        self.sub_sidebar.setMinimumWidth(MIN_WIDTH_SUBSIDEBAR)
         self.sub_sidebar.hide()
         self.splitter.addWidget(self.sub_sidebar)
 
         self.plot_canvas = PlotCanvas(self)
-        self.plot_canvas.setMinimumHeight(500)
+        self.plot_canvas.setMinimumHeight(MIN_HEIGHT_PLOTCANVAS)
         self.splitter.addWidget(self.plot_canvas)
 
         self.console_widget = ConsoleWidget(self)
-        self.console_widget.setMinimumHeight(150)
+        self.console_widget.setMinimumHeight(MIN_HEIGHT_CONSOLE)
         self.splitter.addWidget(self.console_widget)
 
         self.sidebar.sub_side_bar_needed.connect(self.toggle_sub_sidebar)
