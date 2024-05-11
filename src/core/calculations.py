170f1eb605eee348a9f5e943abee7205a62c7eaa
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-05-11 19:03:22 +0300

    calculate_reaction cached

--- src/core/calculations.py
+++ src/core/calculations.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+import numpy as np
 import pandas as pd
 from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
 
@@ -56,24 +59,31 @@ class Calculations(QObject):
         x = reaction_params.get('x')
         function_type = reaction_params.get('function')
         coeffs = reaction_params.get('coeffs', {})
-        upper_bound_coeffs = reaction_params.get('upper_bound_coeffs', {})
-        lower_bound_coeffs = reaction_params.get('lower_bound_coeffs', {})
+        upper_coeffs = reaction_params.get('upper_bound_coeffs', {})
+        lower_coeffs = reaction_params.get('lower_bound_coeffs', {})
+
+        x_range = (np.min(x), np.max(x))
+        coeffs_tuple = (coeffs.get('h'), coeffs.get('z'), coeffs.get('w'))
+        upper_coeffs_tuple = (upper_coeffs.get('h'), upper_coeffs.get('z'), upper_coeffs.get('w'))
+        lower_coeffs_tuple = (lower_coeffs.get('h'), lower_coeffs.get('z'), lower_coeffs.get('w'))
 
         return {
-            'value': (x, function_type, coeffs),
-            'upper_bound': (x, function_type, upper_bound_coeffs),
-            'lower_bound': (x, function_type, lower_bound_coeffs)
+            'value': (x_range, function_type, coeffs_tuple),
+            'upper_bound': (x_range, function_type, upper_coeffs_tuple),
+            'lower_bound': (x_range, function_type, lower_coeffs_tuple)
         }
 
+    @lru_cache(maxsize=128)
     def calculate_reaction(self, reaction_params: tuple):
-        x, function_type, coeffs = reaction_params
+        x_range, function_type, coeffs = reaction_params
+        x = np.linspace(x_range[0], x_range[1], 100)
         result = None
         if function_type == 'gauss':
-            result = cft.gaussian(x, **coeffs)
+            result = cft.gaussian(x, *coeffs)
         elif function_type == 'fraser':
-            result = cft.fraser_suzuki(x, **coeffs)
+            result = cft.fraser_suzuki(x, *coeffs)
         elif function_type == 'ads':
-            result = cft.asymmetric_double_sigmoid(x, **coeffs)
+            result = cft.asymmetric_double_sigmoid(x, *coeffs)
         return result
 
     def diff_function(self, data: pd.DataFrame):
@@ -119,7 +129,8 @@ class Calculations(QObject):
         reaction_params = self.extract_reaction_params(path_keys)
         reaction_results = {key: self.calculate_reaction(params) for key, params in reaction_params.items()}
         for label, y in reaction_results.items():
-            x = reaction_params[label][0]
+            x_range = reaction_params[label][0]
+            x = np.linspace(x_range[0], x_range[1], 100)
             self.plot_reaction_signal.emit(label, [x, y])
 
     def process_highlight_reaction(self, path_keys, _params):
@@ -132,10 +143,12 @@ class Calculations(QObject):
             if reaction in path_keys:
                 reaction_results = {key: self.calculate_reaction(params) for key, params in reaction_params.items()}
                 for label, y in reaction_results.items():
-                    x = reaction_params[label][0]
+                    x_range = reaction_params[label][0]
+                    x = np.linspace(x_range[0], x_range[1], 100)
                     self.plot_reaction_signal.emit(label, [x, y])
             else:
-                value_x = reaction_params['value'][0]
+                value_x_range = reaction_params['value'][0]
+                value_x = np.linspace(value_x_range[0], value_x_range[1], 100)
                 value_y = self.calculate_reaction(reaction_params['value'])
                 self.plot_reaction_signal.emit('value', [value_x, value_y])
         logger.debug(f'Реакции активного файла: {data.keys()}, имя файла: {file_name}, ключи запроса:{path_keys}')
