0c769527ccc987c64707ef992a4233cb7acf174f
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-05-12 12:48:38 +0300

    CoeffsTable added

--- src/gui/main_tab/sub_sidebar/deconvolution_sub_bar.py
+++ src/gui/main_tab/sub_sidebar/deconvolution_sub_bar.py
@@ -1,33 +1,22 @@
 from PyQt6.QtCore import pyqtSignal
-from PyQt6.QtWidgets import (QHBoxLayout, QLabel, QListWidget, QMessageBox,
-                             QPushButton, QVBoxLayout, QWidget)
+from PyQt6.QtWidgets import (QHBoxLayout, QHeaderView, QListWidget,
+                             QMessageBox, QPushButton, QTableWidget,
+                             QTableWidgetItem, QVBoxLayout, QWidget)
 
 from core.logger_config import logger
 
 
-class CalcButtons(QWidget):
+class FileTransferButtons(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.layout = QVBoxLayout(self)
-        self.start_button = QPushButton("Начать расчет")
-        self.stop_button = QPushButton("Остановить расчет")
-        self.layout.addWidget(self.start_button)
 
-        self.start_button.clicked.connect(self.start_calculation)
-        self.stop_button.clicked.connect(self.stop_calculation)
-        self.is_calculating = False
-
-    def start_calculation(self):
-        self.is_calculating = True
-        self.layout.replaceWidget(self.start_button, self.stop_button)
-        self.start_button.hide()
-        self.stop_button.show()
-
-    def stop_calculation(self):
-        self.is_calculating = False
-        self.layout.replaceWidget(self.stop_button, self.start_button)
-        self.stop_button.hide()
-        self.start_button.show()
+        self.load_reactions_button = QPushButton("Импорт")
+        self.export_reactions_button = QPushButton("Экспорт")
+        self.buttons_layout = QHBoxLayout()
+        self.buttons_layout.addWidget(self.load_reactions_button)
+        self.buttons_layout.addWidget(self.export_reactions_button)
+        self.layout.addLayout(self.buttons_layout)
 
 
 class ReactionTable(QWidget):
@@ -39,10 +28,10 @@ class ReactionTable(QWidget):
         self.layout = QVBoxLayout(self)
 
         self.add_reaction_button = QPushButton("Добавить")
-        self.load_reaction_button = QPushButton("Загрузить")
+        self.del_reaction_button = QPushButton("Удалить")
         self.top_buttons_layout = QHBoxLayout()
         self.top_buttons_layout.addWidget(self.add_reaction_button)
-        self.top_buttons_layout.addWidget(self.load_reaction_button)
+        self.top_buttons_layout.addWidget(self.del_reaction_button)
         self.layout.addLayout(self.top_buttons_layout)
 
         self.reactions_list = QListWidget()
@@ -84,14 +73,62 @@ class ReactionTable(QWidget):
             QMessageBox.warning(self, "Настройки Реакции", "Пожалуйста, выберите реакцию из списка.")
 
 
+class CoeffsTable(QTableWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(5, 3, parent)
+        self.setHorizontalHeaderLabels(['low', 'val', 'up'])
+        self.setVerticalHeaderLabels(['h', 'z', 'w', 'a1', 'a2'])
+
+        for i in range(5):
+            for j in range(3):
+                self.setItem(i, j, QTableWidgetItem(f"{i+1},{j+1}{i+1}"))
+
+        row_height = self.rowHeight(0)
+        borders_height = 10
+        header_height = self.horizontalHeader().height()
+        total_height = (row_height * 5) + header_height + borders_height
+        self.setFixedHeight(total_height)
+
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+
+
+class CalcButtons(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.start_button = QPushButton("Начать расчет")
+        self.stop_button = QPushButton("Остановить расчет")
+        self.layout.addWidget(self.start_button)
+
+        self.start_button.clicked.connect(self.start_calculation)
+        self.stop_button.clicked.connect(self.stop_calculation)
+        self.is_calculating = False
+
+    def start_calculation(self):
+        self.is_calculating = True
+        self.layout.replaceWidget(self.start_button, self.stop_button)
+        self.start_button.hide()
+        self.stop_button.show()
+
+    def stop_calculation(self):
+        self.is_calculating = False
+        self.layout.replaceWidget(self.stop_button, self.start_button)
+        self.stop_button.hide()
+        self.start_button.show()
+
+
 class DeconvolutionSubBar(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         layout = QVBoxLayout(self)
 
-        self.calc_buttons = CalcButtons(self)
         self.reactions_table = ReactionTable(self)
+        self.coeffs_table = CoeffsTable(self)
+        self.file_transfer_buttons = FileTransferButtons(self)
+        self.calc_buttons = CalcButtons(self)
 
-        layout.addWidget(QLabel("Деконволюция"))
-        layout.addWidget(self.calc_buttons)
         layout.addWidget(self.reactions_table)
+        layout.addWidget(self.coeffs_table)
+        layout.addWidget(self.file_transfer_buttons)
+        layout.addWidget(self.calc_buttons)
