e16286d9891e442a894326e3abd9ed3df61dee62
kdavjd <105581554+kdavjd@users.noreply.github.com>
2024-05-16 11:52:28 +0300

    generate_default_function_data вынесена в cft

--- src/core/curve_fitting.py
+++ src/core/curve_fitting.py
@@ -3,6 +3,51 @@ import numpy as np
 
 class CurveFitting:
     @staticmethod
+    def generate_default_function_data(df):
+        x = df['temperature'].copy()
+        y_columns = [col for col in df.columns if col != 'temperature']
+        if y_columns:
+            y = df[y_columns[0]]
+            h = 0.3 * y.max()
+            z = x.mean()
+            w = 0.1 * (x.max() - x.min())
+
+            h_lower, h_upper = h * 0.9, h * 1.1
+            w_lower, w_upper = w * 0.9, w * 1.1
+            fr, ads1, ads2 = 1, 1, 1
+
+            result_dict = {
+                "function": "gauss",
+                "x": x.to_numpy(),
+                "coeffs": {
+                    "h": h,
+                    "z": z,
+                    "w": w,
+                    "fr": fr,
+                    "ads1": ads1,
+                    "ads2": ads2
+                },
+                "upper_bound_coeffs": {
+                    "h": h_upper,
+                    "z": z,
+                    "w": w_upper,
+                    "fr": fr,
+                    "ads1": ads1,
+                    "ads2": ads2
+                },
+                "lower_bound_coeffs": {
+                    "h": h_lower,
+                    "z": z,
+                    "w": w_lower,
+                    "fr": fr,
+                    "ads1": ads1,
+                    "ads2": ads2
+                }
+            }
+            return result_dict
+        return {}
+
+    @staticmethod
     def gaussian(x, h, z, w) -> np.ndarray:
         return h * np.exp(-((x - z) ** 2) / (2 * w ** 2))
 
