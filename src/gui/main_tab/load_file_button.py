17dc1fd8c40f460790e23e791c7c130e7aef571e
Myakui <yakovlev.nekit228@mail.ru>
2024-07-15 13:39:14 +0300

    Добавление ruff в проекта(fixes #12)

--- src/gui/main_tab/load_file_button.py
+++ src/gui/main_tab/load_file_button.py
@@ -1,9 +1,16 @@
 import os
 
 from PyQt6.QtCore import QSize, pyqtSignal
-from PyQt6.QtWidgets import (QDialog, QDialogButtonBox, QFileDialog,
-                             QHBoxLayout, QLabel, QLineEdit, QVBoxLayout,
-                             QWidget)
+from PyQt6.QtWidgets import (
+    QDialog,
+    QDialogButtonBox,
+    QFileDialog,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
 from src.core.logger_config import logger
 
@@ -11,12 +18,13 @@ from src.core.logger_config import logger
 class LoadButton(QWidget):
     file_selected = pyqtSignal(tuple)
 
-    file_extensions = 'CSV files (*.csv);;Text files (*.txt)'
+    file_extensions = "CSV files (*.csv);;Text files (*.txt)"
 
     def open_file_dialog(self):
         try:
             file_path, _ = QFileDialog.getOpenFileName(
-                self, 'Open File', os.getenv('HOME', ''), self.file_extensions)
+                self, "Open File", os.getenv("HOME", ""), self.file_extensions
+            )
             if file_path:
                 logger.debug("Выбран файл: %s", file_path)
                 self.pre_load_dialog(file_path)
@@ -27,7 +35,13 @@ class LoadButton(QWidget):
         dialog = PreLoadDialog(file_path, self)
         if dialog.exec():
             self.file_selected.emit(
-                (dialog.file_path(), dialog.delimiter(), dialog.skip_rows(), dialog.columns_names()))
+                (
+                    dialog.file_path(),
+                    dialog.delimiter(),
+                    dialog.skip_rows(),
+                    dialog.columns_names(),
+                )
+            )
 
 
 class PreLoadDialog(QDialog):
@@ -39,8 +53,8 @@ class PreLoadDialog(QDialog):
         self.file_path_edit = QLineEdit(file_path)
         self.columns_names_edit = QLineEdit()
         self.columns_names_edit.setPlaceholderText("temperature, 3, 5, 10")
-        self.delimiter_edit = QLineEdit(',')
-        self.skip_rows_edit = QLineEdit('0')
+        self.delimiter_edit = QLineEdit(",")
+        self.skip_rows_edit = QLineEdit("0")
 
         layout = QVBoxLayout()
 
@@ -57,7 +71,9 @@ class PreLoadDialog(QDialog):
         row_layout.addWidget(self.skip_rows_edit)
         layout.addLayout(row_layout)
 
-        buttons = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
         layout.addWidget(buttons)
